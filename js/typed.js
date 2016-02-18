0ed9db2a3cff1fa2c780a35f7a93c48cc2e7cdfb
b3ngee <v5e0b@ugrad.cs.ubc.ca>
2016-02-17 21:29:20 -0800

    added close button to modals

--- js/typed.js
+++ js/typed.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".element").typed({
-        strings: [" third year student at UBC.", " software engineer in training.", " Biochemistry graduate."],
+        strings: [" software engineer in training.", " Biochemistry graduate."],
         typeSpeed: 20,
         backSpeed: 20,
         backDelay: 1000,
