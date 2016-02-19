000cb0562c3894a37b063ec550568065489e1a9c
b3ngee <v5e0b@ugrad.cs.ubc.ca>
2016-02-19 01:19:44 -0800

    Added more typed sentences

--- js/typed.js
+++ js/typed.js
@@ -1,9 +1,10 @@
 $(function(){
     $(".element").typed({
-        strings: [" software engineer in training.", " Biochemistry graduate."],
+        strings: ["a software engineer in training.", "a Biochemistry graduate.", "a UBC alumni.", "a cooking enthusiast.", "a fan of the Warriors.", 
+        "an exercise admirer.", "a Game of Thrones fan.", "a rock climber."],
         typeSpeed: 20,
         backSpeed: 20,
-        backDelay: 1000,
+        backDelay: 2000,
         loop: true,
         loopCount: false,
         showCursor: true,
