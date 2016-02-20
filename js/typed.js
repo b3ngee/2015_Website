6d2db669ecde5b437c1bcf882d90a99149ab1f9d
b3ngee <v5e0b@ugrad.cs.ubc.ca>
2016-02-19 21:09:23 -0800

    added image to background

--- js/typed.js
+++ js/typed.js
@@ -1,7 +1,7 @@
 $(function(){
     $(".element").typed({
-        strings: ["a software engineer in training.", "a Biochemistry graduate.", "a UBC alumni.", "a cooking enthusiast.", "a fan of the Warriors.", 
-        "an exercise admirer.", "a Game of Thrones fan.", "a rock climber."],
+        strings: ["a software developer.", "a Biochemistry graduate.", "a UBC alumni.", "a cooking enthusiast.", "a fan of the Warriors.", 
+        "an exercise admirer.", "a Game of Thrones fan."],
         typeSpeed: 20,
         backSpeed: 20,
         backDelay: 2000,
